b181edad673199d581d309b9bef81cad1465b632
Idir Lankri <idir.lankri@geneanet.org>
2024-12-20 15:37:31 +0100

    WIP: js: Adapt the glue code for the JavaScript library

--- js/calendars_js.ml
+++ js/calendars_js.ml
@@ -2,7 +2,18 @@ let () =
   Js_of_ocaml.Js.export "Calendars"
     begin
       object%js
-        method make day month year = { Calendars.day; month; year; delta = 0 }
+        method makeGregorian day month year =
+          Calendars.Unsafe.make ~day ~month ~year ~delta:0 Calendars.Gregorian
+
+        method makeJulian day month year =
+          Calendars.Unsafe.make ~day ~month ~year ~delta:0 Calendars.Julian
+
+        method makeFrench day month year =
+          Calendars.Unsafe.make ~day ~month ~year ~delta:0 Calendars.French
+
+        method makeHebrew day month year =
+          Calendars.Unsafe.make ~day ~month ~year ~delta:0 Calendars.Hebrew
+
         method day { Calendars.day; _ } = day
         method month { Calendars.month; _ } = month
         method year { Calendars.year; _ } = year
@@ -11,16 +22,7 @@ let () =
         method julianOfSdn = Calendars.julian_of_sdn
         method frenchOfSdn = Calendars.french_of_sdn
         method hebrewOfSdn = Calendars.hebrew_of_sdn
-        method sdnOfGregorian = Calendars.sdn_of_gregorian
-        method sdnOfJulian = Calendars.sdn_of_julian
-        method sdnOfFrench = Calendars.sdn_of_french
-        method sdnOfHebrew = Calendars.sdn_of_hebrew
-        method gregorianOfJulian = Calendars.gregorian_of_julian
-        method julianOfGregorian = Calendars.julian_of_gregorian
-        method gregorianOfFrench = Calendars.gregorian_of_french
-        method frenchOfGregorian = Calendars.french_of_gregorian
-        method gregorianOfHebrew = Calendars.gregorian_of_hebrew
-        method hebrewOfGregorian = Calendars.hebrew_of_gregorian
+        method toSdn = Calendars.to_sdn
 
         method moonPhaseOfSdn s =
           match Calendars.moon_phase_of_sdn s with
